306aa0d46def053e7f197a0a5eb202123edbc6b2
zneiat <1149527164@qq.com>
2017-10-07 15:40:11 +0800

    Modified the installation.

--- src/settings.php
+++ src/settings.php
@@ -17,15 +17,6 @@ return [
         ],
 
         // Database
-        'db' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'library_register',
-            'username' => 'library_register',
-            'password' => '9ed5e01c',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ]
+        'db' => include(__DIR__ . '/../database-config.php'),
     ],
 ];
